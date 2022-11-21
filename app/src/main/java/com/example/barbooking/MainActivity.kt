c79f5718b73fa1a68e22d9627b5f2eeb5fbc10cf
hypnotizzze <57011045+hypnotizzze@users.noreply.github.com>
2022-11-21 18:24:45 +0300

    First commit

---
+++ app/src/main/java/com/example/barbooking/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.barbooking
+
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+  val TAG: String = "MainActivity"
+
+  lateinit var LoginInput_1: TextInputEditText
+  lateinit var PasswordInput_1: TextInputEditText
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    register_text_2.setOnClickListener {
+      val intent = Intent(this, RegisterActivity::class.java)
+      startActivity(intent)
+    }
+
+    val animationDrawable = constraintLayout.background as AnimationDrawable
+    animationDrawable.apply {
+      setEnterFadeDuration(1000)
+      setExitFadeDuration(2000)
+      start()
+    }
+    initViews()
+  }
+
+  fun initViews() {
+    //val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+    //setSupportActionBar(toolbar)
+
+    LoginInput_1 = findViewById(R.id.text_input_edit_login_1)
+    PasswordInput_1 = findViewById(R.id.text_input_edit_password_1)
+
+  }
+}
---
+++ app/src/main/java/com/example/barbooking/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package com.example.barbooking
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class RegisterActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_register)
+
+    register_text_2.setOnClickListener {
+      val intent = Intent(this, MainActivity::class.java)
+      startActivity(intent)
+    }
+  }
+}
